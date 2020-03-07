658ffd287077d11b458bbdb1424c8b5d6f0a101c
naito036 <iniesutasiesuta24@gmail.com>
2020-03-07 17:42:00 +0900

    error

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,8 +1,20 @@
 class CalcsController < ApplicationController
   def show
-    ＠val1 = prams[:val1]
-    ＠calc = prams[:calc]
-    ＠val2 = prams[:val2]
+    val1 = params[:val1].to_i
+    calc = params[:calc]
+    val2 = params[:val2].to_i
+
+      @result = if calc == "addition"
+                  val1 + val2
+                elsif calc == "subtraction"
+                  val1 - val2
+                elsif calc == "multiplication"
+                  val1 * val2
+                elsif calc == "division"
+                  val1 / val2
+                else
+                  "計算できません"
+                end
     
   end
 end
